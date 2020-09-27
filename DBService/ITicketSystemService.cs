b1d1147b4daef9efc1af2f8479ec165a704dddf4
Ken <ken.roulamellah@gmail.com>
2020-09-27 18:40:36 +0200

    Added services to access projects/tickets of given user

---
+++ DBService/ITicketSystemService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DBService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface ITicketSystemService
+    {
+
+        //[OperationContract]
+        //bool createProject();
+
+        [OperationContract]
+        bool createTicket();
+
+        [OperationContract]
+        DUser connectAsUser(string name, string password);
+
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    [DataContract]
+    public class DUser
+    {
+
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public ObservableCollection<DProject> projectsMember { get; set; }
+
+    }
+
+    [DataContract]
+    public class DTicket
+    {
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Descritpion { get; set; }
+
+        [DataMember]
+        public byte[] Date { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember]
+        public string Creator { get; set; }
+
+        [DataMember]
+        public string Owner { get; set; }
+    }
+
+    [DataContract]
+    public class DProject
+    {
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Owner { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public ObservableCollection<DTicket> Tickets { get; set; }
+    }
+}
---
+++ DBService/TicketSystemService.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DBService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class TicketSystemService : ITicketSystemService
+    {
+        public bool createTicket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetData(int value)
+        {
+            string res = "";
+            using (var entities = new DBEntities())
+            {
+                var u = entities.Users.FirstOrDefault(
+                    (user) => user.Username == "ken");
+
+                foreach(Project p in u.ProjectMember)
+                {
+                    res += ($"Nom : {p.Name}, Prénom : {p.Owner.Username} \n");
+                }
+            }
+            return res;
+        }
+
+        public DUser connectAsUser(string name, string password)
+        {
+            try
+            {
+                using (var entities = new DBEntities())
+                {
+                    ObservableCollection<DProject> projects = new ObservableCollection<DProject>();
+                    var u = entities.Users.First((elem) => elem.Username == name && elem.Password == password);
+                    foreach (Project p in u.ProjectMember)
+                    {
+                        ObservableCollection<DTicket> tickets = new ObservableCollection<DTicket>();
+                        foreach (Ticket t in p.Tickets)
+                        {
+                            tickets.Add(new DTicket()
+                            {
+                                Title = t.Title,
+                                Descritpion = t.Descritpion,
+                                Date = t.Date,
+                                State = t.State.StateName,
+                                Creator =  t.Creator.Username,
+                                Owner = t.Owner.Username
+                            });
+                        }
+
+                        projects.Add(new DProject()
+                        {
+                            Name = p.Name,
+                            Owner = p.Owner.Username,
+                            Description = p.Description,
+                            Tickets = tickets
+                        });
+                    }
+                    return new DUser() { Username = u.Username, projectsMember = projects };
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
